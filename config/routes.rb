1185c7a0c5116240a977e29d3eb24ec55dafe732
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-10-09 10:20:51 +0000

    [Add]ユーザー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'homes#top'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy
-]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get "/homes/about" => "homes#about",as:"about"
 
+  resources :users, only: [:show, :edit, :update]
+  patch '/users/:id' => 'users#update', as: 'update_user'
 end
